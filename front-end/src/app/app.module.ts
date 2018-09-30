16c242289e019e034a48d9696f859ccfab082608
adminjacob <adjamoro@gmail.com>
2018-09-30 18:00:08 -0500

    Primer componente

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ControlComponent } from './components/control/control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ControlComponent
   ],
   imports: [
     BrowserModule
---
+++ front-end/src/app/lib/data.js
@@ -0,0 +1,21 @@
+var ganttData = [
+    {
+            id: 1, name: "Actividad 1", series: [
+                    { name: "SubActividad 1", start: new Date(2018,08,26), end: new Date(2018,09,9),color: "#52FF33" },
+                    { name: "Subactividad 2", start: new Date(2018,08,30), end: new Date(2018,09,12), color: "#52FF33" }
+            ]
+    }, 
+    {
+            id: 2, name: "Actividad 2", series: [
+                    { name: "SubActividad 1", start: new Date(2018,09,00), end: new Date(2018,10,09),color: "#52FF33" },
+                    { name: "SubActividad 2", start: new Date(2018,09,04), end: new Date(2018,10,15), color: "#52FF33" },
+                    { name: "Subactividad 3", start: new Date(2018,09,05), end: new Date(2018,10,07), color: "#52FF33" }
+            ]
+    }, 
+    {
+            id: 3, name: "Actividad 3", series: [
+                    { name: "SubActividad 1", start: new Date(2018,11,30), end: new Date(2018,12,04),color: "#52FF33" },
+                    { name: "SubActividad 2", start: new Date(2018,11,05), end: new Date(2018,12,00), color: "#52FF33" }
+            ]
+    }
+];
