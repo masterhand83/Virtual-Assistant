98aaedc2eefaf716920d391f3ecd64af165852e9
masterhand83 <robinmiguelsdj@gmail.com>
2018-10-30 09:42:08 -0600

    creador de scripts: REORDENAR LIBRERIAS IMPORTADAS

--- front-end/src/app/components/gantt/gantt.component.ts
+++ front-end/src/app/components/gantt/gantt.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionService } from '../../services/session.service';
-import {ProjectsService } from '../../services/projects.service';
+import { ProjectsService } from '../../services/projects.service';
 import { Router } from '@angular/router';
+
+import { Renderer2,Inject } from "@angular/core";
+import { DOCUMENT } from "@angular/platform-browser";
 @Component({
   selector: 'app-gantt',
   templateUrl: './gantt.component.html',
@@ -9,14 +12,38 @@ import { Router } from '@angular/router';
 })
 export class GanttComponent implements OnInit {
 
-  constructor(private sess:SessionService, private projectService:ProjectsService,private router: Router) { }
-
+  constructor(
+    private sess: SessionService, 
+    private projectService: ProjectsService, 
+    private router: Router,
+    private _Renderer2: Renderer2,
+    @Inject(DOCUMENT) private _document
+    ) { }
+  script: string;
   ngOnInit() {
-   this.sess.validateProject();
-   this.getUserType();
-   this.getIdProject();
+    let s = this._Renderer2.createElement('script');
+    s.text = `
+    var i = 'hola mundo'
+    alert(i)
+    `
+    this._Renderer2.appendChild(this._document.body,s);
+
+
+
+
+
+
+    this.sess.validateProject();
+    this.getUserType();
+    this.getIdProject();
+    this.generateScript();
   }
-  
+  generateScript(){
+    this.script = `<script></script>`
+  }
+
+
+
   deleteProject(_id: string) {
     if (confirm('¿Estas seguro de eliminarlo?')) {
       this.projectService.deleteProject(_id)
@@ -24,7 +51,7 @@ export class GanttComponent implements OnInit {
           this.sess.deleteProjectSession();
           this.router.navigate([('/projects')]);
           alert('Eliminado Exitosamente');
-          
+
         });
     }
 
@@ -32,28 +59,28 @@ export class GanttComponent implements OnInit {
 
 
 
-  key:string;
-  userTypeBoolean:boolean=false;
-  userTypeBoolean2:boolean=false;
-  userType:string;
-  getUserType(){
-    this.key="UserType";
-    this.userType=this.sess.getFromSession(this.key);
-    if(this.userType=="1"){
-      this.userTypeBoolean=true;
+  key: string;
+  userTypeBoolean: boolean = false;
+  userTypeBoolean2: boolean = false;
+  userType: string;
+  getUserType() {
+    this.key = "UserType";
+    this.userType = this.sess.getFromSession(this.key);
+    if (this.userType == "1") {
+      this.userTypeBoolean = true;
     }
-    else if(this.userType=="2"){
-      this.userTypeBoolean2=true;
+    else if (this.userType == "2") {
+      this.userTypeBoolean2 = true;
     }
-    
-    
+
+
   }
 
-  key2:string;
-  _id:string;
-  getIdProject(){
-    this.key2="ActualProject";
-    this._id=this.sess.getFromSession(this.key2);
+  key2: string;
+  _id: string;
+  getIdProject() {
+    this.key2 = "ActualProject";
+    this._id = this.sess.getFromSession(this.key2);
   }
 
 
--- front-end/src/app/components/messages/messages.component.ts
+++ front-end/src/app/components/messages/messages.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ProjectsService} from '../../services/projects.service';
-import {Project} from '../../models/Project';
+import { ProjectsService } from '../../services/projects.service';
+import { Project } from '../../models/Project';
 import { SessionService } from '../../services/session.service';
 
 
@@ -11,44 +11,45 @@ import { SessionService } from '../../services/session.service';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor(private projectsService:ProjectsService,private sess:SessionService) { }
+  constructor(private projectsService: ProjectsService, private sess: SessionService) { }
 
   ngOnInit() {
     this.getUserType();
     this.getProjects();
-    
+
   }
 
-  
-  getProjects(){
+
+  getProjects() {
 
 
-    if(this.userType=="1"){
+    if (this.userType == "1") {
       this.projectsService.getProjects()
-      .subscribe(res => {
-        this.projectsService.project2=res as Project[];
-      
-      });   
+        .subscribe(res => {
+          this.projectsService.project2 = res as Project[];
+
+        });
     }
-    else if(this.userType=="2" || this.userType=="3"){
+    else if (this.userType == "2" || this.userType == "3") {
       this.projectsService.getProjectsRP(this.UserID)
-      .subscribe(res=>{
-        this.projectsService.project2=res as Project[];
-      })
+        .subscribe(res => {
+          this.projectsService.project2 = res as Project[];
+        })
     }
   }
 
-  key:string;
-  key2:string;
-  UserID:string;
-  
-  userType:string;
-  getUserType(){
-    this.key="UserType";
-    this.userType=this.sess.getFromSession(this.key);
-    
-    this.key2="UserID";
-    this.UserID=this.sess.getFromSession(this.key2);
-    
+  key: string;
+  key2: string;
+  UserID: string;
 
+  userType: string;
+  getUserType() {
+    this.key = "UserType";
+    this.userType = this.sess.getFromSession(this.key);
+
+    this.key2 = "UserID";
+    this.UserID = this.sess.getFromSession(this.key2);
+
+
+  }
 }
